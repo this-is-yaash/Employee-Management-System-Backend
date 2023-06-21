c212bede39d20164bf59d985cbce1185f9d7f6b4
this-is-yaash <yashwanth1715@gmail.com>
2023-06-21 14:49:45 +0530

    stablesofar

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -2,10 +2,8 @@
 
 namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
-use App\Models\AdminDetails;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
 class AuthController extends Controller
 {
     //
