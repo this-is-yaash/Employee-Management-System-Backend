4ce9edb3ee52a89206176ca8984eb6c726894b59
Yashwanth <84762257+this-is-yaash@users.noreply.github.com>
2023-06-22 16:15:52 +0530

    Revert "merge"

--- database/migrations/2019_08_19_000000_create_failed_jobs_table.php
+++ database/migrations/2019_08_19_000000_create_failed_jobs_table.php
@@ -13,11 +13,14 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-
-        
-            $table->string('email')->unique();
-            $table->string('password');
+        Schema::create('failed_jobs', function (Blueprint $table) {
+            $table->id();
+            $table->string('uuid')->unique();
+            $table->text('connection');
+            $table->text('queue');
+            $table->longText('payload');
+            $table->longText('exception');
+            $table->timestamp('failed_at')->useCurrent();
         });
     }
 
@@ -28,6 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('failed_jobs');
     }
 };
--- resources/views/emp_dashboard.blade.php
+++ resources/views/emp_dashboard.blade.php
@@ -38,9 +38,93 @@
   <div class="col-10">
     <div><h1>Employee Details</h1></div>
     <a href="{{route('register')}}"><button class="btn btn-secondary" id="addemp">Add Employee</button></a>
-    @yield('tablecontent')
+        <button class="btn btn-secondary" id="delemp">Delete Employee</button>
 
-</div>
+      <table id="table" class="center" style="width: 100%">
+        <thead>
+          <th>Employee Name</i></th>
+          <th>Designation</i></th>
+          <th>Phone Number</i></th>
+          <th>Salary </i></th>
+          <th>Age</i></th>
+          <th>Date of Birth</i></th>
+        </thead >
+        <tr>
+          <td>John Harvey</td>
+          <td>Web Developer</td>
+          <td>1234567890</td>
+          <td>xyz$</td>
+          <td>22</td>
+          <td>12/12/2000</td>
+        </tr>
+        <tr>
+          <td>Mark Luther</td>
+          <td>Web Developer</td>
+          <td>1234567890</td>
+          <td>xyz$</td>
+          <td>21</td>
+          <td>12/10/2001</td>
+        </tr>
+        <tr>
+          <td>Ben Johnson</td>
+          <td>Web Designer</td>
+          <td>1234567890</td>
+          <td>xyz$</td>
+          <td>23</td>
+          <td>15/08/1999</td>
+        </tr>
+        <tr>
+          <td>Sarah Connor</td>
+          <td>Tester</td>
+          <td>1234567890</td>
+          <td>xyz$</td>
+          <td>25</td>
+          <td>12/06/1997</td>
+        </tr>
+        <tr>
+          <td>Hannah</td>
+          <td>Debugger</td>
+          <td>1234567890</td>
+          <td>xyz$</td>
+          <td>22</td>
+          <td>02/10/2000</td>
+        </tr>
+        <tr>
+          <td>John Smith</td>
+          <td>Android Developer</td>
+          <td>1234567890</td>
+          <td>xyz$</td>
+          <td>23</td>
+          <td>11/02/1999</td>
+        </tr>
+        <tr>
+          <td>Will Smith</td>
+          <td>iOS Developer</td>
+          <td>1234567890</td>
+          <td>xyz$</td>
+          <td>24</td>
+          <td>20/11/1998</td>
+        </tr>
+        <tr>
+          <td>John Harvey</td>
+          <td>Web Developer</td>
+          <td>1234567890</td>
+          <td>xyz$</td>
+          <td>22</td>
+          <td>12/12/2000</td>
+        </tr>
+        </tbody>
+      </table>
+      <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+      <script src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
+      <script src="https://cdn.datatables.net/1.13.1/js/dataTables.bootstrap5.min.js"></script>
+      <script>
+        $(document).ready(function () {
+            $('#table').DataTable();
+
+          });
+      </script>
+  </div>
   </div>
 
   </body>
